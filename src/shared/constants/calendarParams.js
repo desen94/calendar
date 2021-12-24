8d3344d9a8cbfeee291174e446a8402bcfd0537c
desen <32864798+desen94@users.noreply.github.com>
2021-12-24 15:16:06 +0500

    refactor: remove magic numbers to constants

---
+++ src/shared/constants/calendarParams.js
@@ -0,0 +1,4 @@
+export const CALENDAR_PARAMS = {
+    ROWS: 5,
+    COLUMNS: 7
+}
--- src/utils/generateDays.js
+++ src/utils/generateDays.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
+import {CALENDAR_PARAMS} from '../shared/constants/calendarParams'
 
 dayjs.locale('ru')
 
@@ -9,8 +10,8 @@ export function getMonth(date = dayjs()) {
     const firstDayOfTheMonth = dayjs(new Date(year, month, 0)).day()
     let currentMonthCount = 0 - firstDayOfTheMonth
     
-    return new Array(5).fill([]).map(() => {
-        return new Array(7).fill(null).map(() => {
+    return new Array(CALENDAR_PARAMS.ROWS).fill([]).map(() => {
+        return new Array(CALENDAR_PARAMS.COLUMNS).fill(null).map(() => {
             currentMonthCount++
             return dayjs(new Date(year, month, currentMonthCount))
         })
